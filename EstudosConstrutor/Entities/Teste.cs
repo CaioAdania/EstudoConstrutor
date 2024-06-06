522652dd0ee3897e3b073bad48b9f7486ef18e5a
CaioAdania <caioadania@gmail.com>
2024-06-06 15:19:54 -0300

    Estudos Construtores

---
+++ EstudosConstrutor/Entities/Teste.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosConstrutor.Entities
+{
+    internal class Teste
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Teste(string nome, double preco,int quantidade) //construtor
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ EstudosConstrutor/Program.cs
@@ -0,0 +1,36 @@
+﻿using EstudosConstrutor.Entities;
+using System.Globalization;
+
+namespace EstudosConstrutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Teste t = new Teste(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + t);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado: ");
+            int adcQtde = int.Parse(Console.ReadLine());
+            t.AdicionarProdutos(adcQtde);
+            Console.WriteLine(t);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido: ");
+            int rtrQtde = int.Parse(Console.ReadLine());
+            t.RemoverProdutos(rtrQtde);
+            Console.WriteLine(t);
+        }
+    }
+}
